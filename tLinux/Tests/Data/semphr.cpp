71f8ecfdb6fb8c0c07fd44fcba69f7113a0d446a
Djones A. Boni <djboni@gmail.com>
2016-07-11 00:30:42 -0300

    test: tLinux Semaphore fix

--- tLinux/Tests/Data/semphr.cpp
+++ tLinux/Tests/Data/semphr.cpp
@@ -19,8 +19,8 @@ struct SemaphoreFixture {
 		BOOST_CHECK_EQUAL(semaphore.Count, i);
 	}
 
-	void do_lockall() {
-		while(Semaphore_lock(&semaphore) != 0U) {}
+	void do_unlockall() {
+		while(Semaphore_unlock(&semaphore) != 0U) {}
 	}
 };
 
@@ -30,7 +30,7 @@ BOOST_AUTO_TEST_CASE(fixture_should_initialize_semaphore)
 {
 	/* Fixture should initialize semaphore. */
 
-    BOOST_CHECK_EQUAL(semaphore.Count, count);
+    BOOST_CHECK_EQUAL(semaphore.Count, 0U);
     BOOST_CHECK_EQUAL(semaphore.Max, count);
 }
 
@@ -39,36 +39,16 @@ BOOST_AUTO_TEST_CASE(initialize_binary_semaphore)
 	/* Init binary should initialize semaphore with count and max to one. */
 
 	Semaphore_initbinary(&semaphore);
-    BOOST_CHECK_EQUAL(semaphore.Count, 1U);
+    BOOST_CHECK_EQUAL(semaphore.Count, 0U);
     BOOST_CHECK_EQUAL(semaphore.Max, 1U);
 
     Semaphore_init(&semaphore, count); /* Undo modifications. */
-}
-
-BOOST_AUTO_TEST_CASE(lock_should_succeed_when_not_empty)
-{
-	/* Should succeed when not empty. */
-
-	check_count(count);
-
-	for(size_t i = 0; i < count; ++i)
-	{
-		BOOST_CHECK_EQUAL(Semaphore_lock(&semaphore), 1U);
-
-		check_count(count - (i + 1U));
-	}
-
-	BOOST_CHECK_EQUAL(Semaphore_lock(&semaphore), 0U);
 }
 
 BOOST_AUTO_TEST_CASE(unlock_should_succeed_when_not_full)
 {
 	/* Should succeed when not full. */
 
-	BOOST_CHECK_EQUAL(Semaphore_unlock(&semaphore), 0U);
-	check_count(count);
-
-	do_lockall();
 	check_count(0U);
 
 	for(size_t i = 0; i < count; ++i)
@@ -79,21 +59,41 @@ BOOST_AUTO_TEST_CASE(unlock_should_succeed_when_not_full)
 	}
 
 	BOOST_CHECK_EQUAL(Semaphore_unlock(&semaphore), 0U);
+}
+
+BOOST_AUTO_TEST_CASE(lock_should_succeed_when_not_empty)
+{
+	/* Should succeed when not empty. */
+
+	BOOST_CHECK_EQUAL(Semaphore_lock(&semaphore), 0U);
+	check_count(0U);
+
+	do_unlockall();
 	check_count(count);
+
+	for(size_t i = 0; i < count; ++i)
+	{
+		BOOST_CHECK_EQUAL(Semaphore_lock(&semaphore), 1U);
+
+		check_count(count - (i + 1U));
+	}
+
+	BOOST_CHECK_EQUAL(Semaphore_lock(&semaphore), 0U);
+	check_count(0U);
 }
 
 BOOST_AUTO_TEST_CASE(functions_getcount_getmax)
 {
 	/* Should return the expected values. */
 
-	BOOST_CHECK_EQUAL(Semaphore_getcount(&semaphore), count);
+	BOOST_CHECK_EQUAL(Semaphore_getcount(&semaphore), 0U);
 	BOOST_CHECK_EQUAL(Semaphore_getmax(&semaphore), count);
 
 	for(size_t i = 0; i < count; ++i)
 	{
-		Semaphore_lock(&semaphore);
+		Semaphore_unlock(&semaphore);
 
-		BOOST_CHECK_EQUAL(Semaphore_getcount(&semaphore), count - (i + 1));
+		BOOST_CHECK_EQUAL(Semaphore_getcount(&semaphore), (i + 1U));
 		BOOST_CHECK_EQUAL(Semaphore_getmax(&semaphore), count);
 	}
 }
