6a1f268a9a6d3f44a6de05bccce141edcfa09616
Djones A. Boni <djboni@gmail.com>
2016-07-12 19:12:53 -0300

    test: Run test on script
    The post-build will not run if the executable is not recompiled.
    Jenkins complains about old/missing test reports.

--- scripts/build_tLinux.sh
+++ scripts/build_tLinux.sh
@@ -5,12 +5,12 @@ cd tLinux/Debug
 
 make all
 
-#mkdir -p test-reports
-#./tLinux.elf \
-#        --log_format=XML \
-#        --log_level=all \
-#        --report_level=short \
-#        --log_sink=test-reports/report.xml
+mkdir -p test-reports
+./tLinux.elf \
+        --log_format=XML \
+        --log_level=all \
+        --report_level=short \
+        --log_sink=test-reports/report.xml
 
 # --log_level=all|success|test_suite|message|warning|error|cpp_exception|
 #             system_error|fatal_error|nothing
