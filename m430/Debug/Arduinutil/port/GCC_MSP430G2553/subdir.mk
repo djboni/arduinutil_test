14c5a3249375c7475b86b1edc147a82604f1c3b4
Djones A. Boni <djboni@gmail.com>
2016-08-03 09:38:28 -0300

    embedded: MSP430 serial

--- m430/Debug/Arduinutil/port/GCC_MSP430G2553/subdir.mk
+++ m430/Debug/Arduinutil/port/GCC_MSP430G2553/subdir.mk
@@ -5,22 +5,25 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Arduinutil/port/GCC_MSP430G2553/Arduinutil.c \
-../Arduinutil/port/GCC_MSP430G2553/Digital.c 
+../Arduinutil/port/GCC_MSP430G2553/Digital.c \
+../Arduinutil/port/GCC_MSP430G2553/Serial.c 
 
 OBJS += \
 ./Arduinutil/port/GCC_MSP430G2553/Arduinutil.o \
-./Arduinutil/port/GCC_MSP430G2553/Digital.o 
+./Arduinutil/port/GCC_MSP430G2553/Digital.o \
+./Arduinutil/port/GCC_MSP430G2553/Serial.o 
 
 C_DEPS += \
 ./Arduinutil/port/GCC_MSP430G2553/Arduinutil.d \
-./Arduinutil/port/GCC_MSP430G2553/Digital.d 
+./Arduinutil/port/GCC_MSP430G2553/Digital.d \
+./Arduinutil/port/GCC_MSP430G2553/Serial.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 Arduinutil/port/GCC_MSP430G2553/%.o: ../Arduinutil/port/GCC_MSP430G2553/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	msp430-gcc -std=c90 -ansi -D__MSP430G2553__ -I../Source -I../../a328P/Arduinutil -I../../a328P/Arduinutil/port/GCC_MSP430G2553 -Os -g3 -pedantic -pedantic-errors -Wall -Wextra -Werror -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	msp430-gcc -std=c90 -ansi -DF_CPU=1000000UL -I../Source -I../../a328P/Arduinutil -I../../a328P/Arduinutil/port/GCC_MSP430G2553 -Os -g3 -pedantic -pedantic-errors -Wall -Wextra -Werror -mmcu=msp430g2553 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- m430/Debug/Source/subdir.mk
+++ m430/Debug/Source/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 Source/%.o: ../Source/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	msp430-gcc -std=c90 -ansi -D__MSP430G2553__ -I../Source -I../../a328P/Arduinutil -I../../a328P/Arduinutil/port/GCC_MSP430G2553 -Os -g3 -pedantic -pedantic-errors -Wall -Wextra -Werror -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	msp430-gcc -std=c90 -ansi -DF_CPU=1000000UL -I../Source -I../../a328P/Arduinutil -I../../a328P/Arduinutil/port/GCC_MSP430G2553 -Os -g3 -pedantic -pedantic-errors -Wall -Wextra -Werror -mmcu=msp430g2553 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
